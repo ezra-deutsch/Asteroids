6a77e4630794b4588e0cc3183c6208e8b1f63e64
Ezra Deutsch <ezdeutsch@gmail.com>
2026-07-29 16:51:20 -0400

    commit rate limit

--- player.py
+++ player.py
@@ -1,14 +1,14 @@
 import pygame
 from shot import Shot
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
+        self.shoot_cooldown = 0
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -45,8 +45,11 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.shoot_cooldown <= 0.0:
             self.shoot()
+            self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+        self.shoot_cooldown -= dt
+            
 
 
 
