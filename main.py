53f4d6270b0f9109eff7d8a96866437ba6d2d86d
Ezra Deutsch <ezdeutsch@gmail.com>
2026-07-29 13:21:37 -0400

    added game loop code to main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT 
+from logger import log_state
+
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH},Screen height: {SCREEN_HEIGHT}")
+
+
+    # Initialize all imported pygame modules
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        screen.fill('black')  # Clear the screen with black before drawing each frame
+        pygame.display.flip()  # Update the full display surface to the screen
+        for event in pygame.event.get():
+
+
+            pass
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
 if __name__ == "__main__":
     main()
